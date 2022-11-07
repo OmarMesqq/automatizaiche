83685e8a8637950b9709a074d22a1cd149ba214e
Omar <34662089+OmarMesqq@users.noreply.github.com>
2022-11-08 05:51:19 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# Automatizaiche 
+#
+# Projeto Piloto de Hardskills da AIChE UFRJ
+#   Omar Mesquita
+#   Gabriel Bohrer 
+#
+#
+#
+# Roteiro de automação de mensagens para o Processo Seletivo da AIChE UFRJ
+
+
+# Soft depends
+# Geralmente, são bibliotecas 
+# inclusas nos imports principais mas que 
+# não necessariamente são incluídas na 
+# instalação padrão. 
+# openpyxl (para pandas ler arquivos .xlsx)
+# tk ou TkInter (em sistemas Linux, para a interface gráfica do pywhatkit)
+
+# Hard depends
+
+import pandas as pd 
+import pywhatkit as pwk
+from time import sleep 
+
+db = pd.read_excel("dados.xlsx") # Criação do database (db) oriundo dos dados alimentados 
+                                 # pela planilha Excel
+
+a = db.iloc[:,0]  # Variável utilizada para determinar quantos nomes há na lista de candidates
+
+for i in range(len(a)): 
+    
+    nome = db.iloc[i,0]
+    tel  = db.iloc[i,1]
+    sit  = db.iloc[i,2]
+
+
+    texto = "Isso é um teste. Ignore. oi, {}, tudo bem? Viemos dizer que você foi {}".format(nome, sit)
+
+
+    if sit == 'Aprovade':
+
+            pwk.sendwhatmsg_instantly(
+   
+                phone_no = "+{}".format(tel), 
+                message = texto,
+                #wait_time = 5
+                tab_close = True
+            )
+            sleep(20) # Esse é o tempo médio para que o programa envie a mensagem no navegador
